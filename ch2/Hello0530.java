f90dd728de8235bfa608560322c5ecbcce97b11b
이상용 <lsy3709@naver.com>
2025-05-30 10:49:59 +0900

    2교시, 클래스에 정의한 정적 메소드를 다른 클래스에서 불러와서 사용해보기 퀴즈 확인

--- ch2/Hello0530.java
+++ ch2/Hello0530.java
@@ -14,5 +14,10 @@ public class Hello0530 {
         // 퀴즈 1
         // Introduce 클래스에, 정의한 사칙연산 메소드를 이용해서, 각각 계산 결과를
         // result1, result2, result3, result4 변수에 담아서, 출력해보기
+        int result1 = Introduce.sum(100, 200);
+        System.out.println("sum 이라는 함수 이용해서 결과 출력 : " + result1);
+
+        int result2 = Introduce.mul(10, 20);
+        System.out.println("mul 이라는 함수 이용해서 결과 출력 : " + result2);
     }
 }
---
+++ ch2/ScannerTest.java
@@ -0,0 +1,7 @@
+package ch2;
+
+public class ScannerTest {
+    public static void main(String[] args) {
+        System.out.println("ScannerTest 시작");
+    }
+}
