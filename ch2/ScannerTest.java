1661b6b36966018be245b3809325f5c8a8c2ffbd
이상용 <lsy3709@naver.com>
2025-05-30 11:20:01 +0900

    3교시, Scanner 클래스를 이용한 , 콘솔에서 키보드 입력을 한 후, 출력해보기1, 숫자 좋아하는 숫자 예시2

--- ch2/ScannerTest.java
+++ ch2/ScannerTest.java
@@ -35,16 +35,27 @@ public class ScannerTest {
         // 예제 코드
         // 기본 모양,
         // [클래스명] [변수명] = new [클래스명]();
-        // 실제 작업 순서1
+        // 실제 작업 순서1: 최초에 한번만 실행 하면 됨.
         Scanner scanner = new Scanner(System.in);
         // System.in : 표준 입력 스트림, 키보드 입력을 받기 위해 사용
+
         // 실제 작업 순서2
         System.out.println("이름을 입력하세요: ");
         // 실제 작업 순서3
         String name = scanner.nextLine(); // nextLine() 메서드를 사용하여 한 줄 전체를 입력받음
         // 실제 작업 순서4
         System.out.println("입력한 이름: " + name); // 입력받은 이름을 출력
-        // 실제 작업 순서5
+
+        // 퀴즈1,
+        // 본인의 좋아하는 숫자를 입력 받아보기.
+        // 실제 작업 순서2
+        System.out.println("본인의 좋아하는 숫자를 입력하세요: ");
+        // 실제 작업 순서3
+        int number = scanner.nextInt(); // nextLine() 메서드를 사용하여 한 줄 전체를 입력받음
+        // 실제 작업 순서4
+        System.out.println("입력한 본인의 좋아하는 숫자: " + number); // 입력받은 이름을 출력
+
+        // 실제 작업 순서5, 위에서 입력 받는 내용이 전부 작업 후 , 맨 마지막에 한번만 실행.
         scanner.close(); // Scanner 객체를 사용한 후에는 반드시 닫아줘야 함. 리소스 누수 방지
     }
 }
