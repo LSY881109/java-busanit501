241a7bc42e2d9b45d861e2e63253728ad7301b41
이상용 <lsy3709@naver.com>
2025-06-02 16:23:17 +0900

    7교시, 배열을 이용한 회원관리 프로그램1(CRUD), 2 배열에 회원 정보 추가

--- ch3/Exs_ch3_2_user_array_doc.java
+++ ch3/Exs_ch3_2_user_array_doc.java
@@ -1,5 +1,7 @@
 package ch3;
 
+import java.util.Scanner;
+
 public class Exs_ch3_2_user_array_doc {
     // 회원 관리 프로그램 버전2, 저장소: 임시 메모리(배열) 이용할 예정.
     // 배열 주의 사항) 1) 고정 길이 , 2) 크기 변경 불가, 3) 같은 타입 사용.
@@ -13,4 +15,21 @@ public class Exs_ch3_2_user_array_doc {
     static String[] passwords = new String[MAX_USERS];
     static String[] registrationDates = new String[MAX_USERS];
     static int userCount = 0; // 현재 등록된 회원 수
+
+    // 회원 추가 메서드
+    public static void addUser(Scanner scanner) {
+        if (userCount < MAX_USERS) {
+            System.out.println("이름을 입력하세요: ");
+            String name = scanner.nextLine();
+            System.out.println("이메일을 입력하세요: ");
+            String email = scanner.nextLine();
+            System.out.println("패스워드를 입력하세요: ");
+            String password = scanner.nextLine();
+            String registrationDate = util.date.DateUtil.getCurrentDateTime(); // 현재 날짜와 시간
+            userCount++;
+            System.out.println("회원이 추가되었습니다: " + name + ", " + email + ", " + registrationDate);
+        } else {
+            System.out.println("회원 수가 최대치를 초과했습니다.");
+        }
+    }
 }
