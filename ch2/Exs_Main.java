b4f80cc64f0f841693cca27cdff1e463a71b7e03
이상용 <lsy3709@naver.com>
2025-06-02 14:01:00 +0900

    5교시, 스위치 또는 다중 if 문 퀴즈 확인2, 결과 문자열 받아서, 출력 하기 누락 추가.

--- ch2/Exs_Main.java
+++ ch2/Exs_Main.java
@@ -11,13 +11,14 @@ public class Exs_Main {
         System.out.println("좋아하는 월을 입력하세요 (1~12):");
         String month = scanner.nextLine();
         int monthInt = Integer.parseInt(month);
-        // 스캐너 이용해서 받아서 입력.
-        Exs_doc.exMini_quiz1(name, monthInt);
+        // 스캐너 이용해서 받아서 입력.d
+        String result_exMini_quiz1 = Exs_doc.exMini_quiz1(name, monthInt);
+        System.out.println(result_exMini_quiz1);
         scanner.close();
         // 하드 코딩, 직접 이름과, 월을 입력 하건.
         // Exs_doc.exMini_quiz1("홍길동", 5);
         // 퀴즈
-
+        System.out.println("----------------------------");
         // EX2-9, 삼항 연산자 예시
         String result_ex2_9 = Exs_doc.getTernaryExam(75);
         System.out.println("EX2-9, 삼항 연산자 예시: " + result_ex2_9);
