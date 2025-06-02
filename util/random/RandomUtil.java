c1b6baaba48eed100e3e5d97e885d36b0f65ea3c
이상용 <lsy3709@naver.com>
2025-06-02 10:27:06 +0900

    2교시, 랜덤 클래스 기능 예제 확인2, 특정 범위의 랜덤한 정수 하나를 뽑는 기능 확인

--- util/random/RandomUtil.java
+++ util/random/RandomUtil.java
@@ -2,13 +2,14 @@ package util.random;
 
 public class RandomUtil {
     // 설계 클래스
-    // Math 클래스에 있는 random() 메소드를 사용하여
-    // 0.0 이상 1.0 미만의 난수를 생성하는 메소드를 작성
-    // 기본 기능 확인용, 정적 메서드 추가해보기.
     public static double generateRandom() {
-        // 데이터 타입 : double, 변수명 : randomValue
-        // Math 클래스의 정적 메소드 random()를 호출하여
         double randomValue = Math.random();
+        return randomValue;
+    }
+
+    // min : 1, max : 10 , 1<= randomValue <= 10, 랜덤 정수 뽑기 기능
+    public static int generateRandomInRange(int min, int max) {
+        int randomValue = (int) (Math.random() * (max - min + 1)) + min;
         return randomValue;
     }
 }
