ca5150a29b24d217f54381a66513677c955576f9
이상용 <lsy3709@naver.com>
2025-06-09 16:37:18 +0900

    7교시, 상속 기본 개념 5, 업캐스팅, 다형성, instanceof 원래의 형으로 변환 확인

--- d250609/ch5_3/GalaxyEx1_3.java
+++ d250609/ch5_3/GalaxyEx1_3.java
@@ -8,4 +8,16 @@ public class GalaxyEx1_3 extends PhoneEx1_2 {
     public void logoInfo() {
         System.out.println("로고 모양 : " + logo);
     }
+    // getter 만들고,
+
+    public String getLogo() {
+        return logo;
+    }
+
+    // 생성자 만들기., 우클릭으로 반자동으로 만들기 중.
+    public GalaxyEx1_3(String company, String name, String price, String logo) {
+        super(company, name, price);
+        this.logo = logo;
+    }
+
 }
--- d250609/ch5_3/IphoeEx1_4.java
+++ d250609/ch5_3/IphoeEx1_4.java
@@ -7,4 +7,16 @@ public class IphoeEx1_4 extends PhoneEx1_2 {
     public void sellInfo() {
         System.out.println("판매처 : " + sellPlace);
     }
+
+    // 생성자 만들기.
+    public IphoeEx1_4(String sellPlace) {
+        // super() 기본 생성자 호출 생략이됨 (부모꺼) , 묵시적 , 생략
+        this.sellPlace = sellPlace;
+    }
+
+    // 매개변수 4개인 생성자를 한번에 다 만들기.
+    public IphoeEx1_4(String company, String name, String price, String sellPlace) {
+        super(company, name, price); // 명시적으로, 표기함.
+        this.sellPlace = sellPlace;
+    }
 }
