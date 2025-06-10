c4c5873e2ae8d8af1022a3e75b4446d3ea308c9d
이상용 <lsy3709@naver.com>
2025-06-10 16:22:20 +0900

    7교시, 컬렉션, List, Set, Map , ArrayList 기본 예시2

--- d250610/ch7/Collection_Ex1.java
+++ d250610/ch7/Collection_Ex1.java
@@ -1,6 +1,9 @@
 package d250610.ch7;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class Collection_Ex1 {
     // Collection 프레임워크의 구조 (인터페이스 와 클래스 구현 관계)
@@ -16,9 +19,11 @@ public class Collection_Ex1 {
     // Map : 데이터 형태 , 키-값 형태 , 키 중복 X, 예) HashMap
 
     public static void main(String[] args) {
+        // 1
         // List 예시, ArrayList , 배열의 업그레이드 버전, 크기 가변.
         // A a = new A(); , A: 클래스명
         // <> 다이어몬드 연산자, 제너릭으로 표현하는데, 쉽게, 저는 여기 <String> 타입만 취급함.
+        // 생성
         ArrayList<String> list = new ArrayList<>();
         // 리스트에 추가
         list.add("사과");
@@ -30,5 +35,43 @@ public class Collection_Ex1 {
         for (String string : list) {
             System.out.println("list 요소 전체 반복문으로 출력 해보기 : " + string);
         }
+        System.out.println("======================================================");
+
+        // 2
+        // HashSet
+        // 생성
+        HashSet<Integer> set = new HashSet<>();
+        // 추가
+        set.add(1); // 기본형으로 1을 추가 -> 사실상 -> 기본형 -> 참조형 : 오토박싱.
+        set.add(2);
+        set.add(1);
+        set.add(3);
+        // 하나 조회 , 존재 여부 확인.
+        System.out.println("하나 조회 : " + set.contains(1));
+        // 전체 조회
+        for (Integer integer : set) {
+            System.out.println("반복문을 이용해서 집합에 담아져 있는 값을 출력해보기 : " + set);
+        }
+        System.out.println("======================================================");
+        // 3
+        // HashMap
+        // 생성.
+        HashMap<String, Integer> map = new HashMap<>();
+        // 추가
+        map.put("a", 100);
+        map.put("b", 200);
+        map.put("c", 300);
+        // 하나 조회,
+        System.out.println("키가 a인 요소의 값을 조회 : " + map.get("a"));
+        // 반복문 이용해서 전체 조회,
+        for (String key : map.keySet()) {
+            System.out.println("반복문 이용해서 전체 조회 : " + map.get(key));
+        }
+        // entrySet(), 키와 값을 같이 구하는 형식.
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(" 키의 값 : " + entry.getKey() + ", value의 값: " + entry.getValue());
+
+        }
+        System.out.println("======================================================");
     }
 }
