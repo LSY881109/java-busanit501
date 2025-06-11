0a088c6811bdf051f4f6f786be11a3f7b8ad7a78
이상용 <lsy3709@naver.com>
2025-06-11 10:28:15 +0900

    2교시, 회원 관리 프로그램 컬렉션 버전, 컬렉션 변경 준비 작업3 , 기존 기능 확인,

--- d250611/ui/MemberUI.java
+++ d250611/ui/MemberUI.java
@@ -2,7 +2,7 @@ package d250611.ui;
 
 import java.util.Scanner;
 
-import ch3.Exs_ch3_2_user_array_doc;
+import d250611.service.UserService;
 
 public class MemberUI {
     public static void drawUI(Scanner scanner) {
@@ -18,23 +18,23 @@ public class MemberUI {
             scanner.nextLine(); // 개행 문자 제거
             switch (menu) {
                 case 1:
-                    Exs_ch3_2_user_array_doc.addUser(scanner); // 회원 추가
+                    UserService.addUser(scanner); // 회원 추가
                     break;
                 case 2:
-                    Exs_ch3_2_user_array_doc.viewUsers(); // 회원 조회
+                    UserService.viewUsers(); // 회원 조회
                     break;
                 case 3:
-                    Exs_ch3_2_user_array_doc.updateUser(scanner); // 회원 수정
+                    UserService.updateUser(scanner); // 회원 수정
                     break;
                 case 4:
-                    Exs_ch3_2_user_array_doc.deleteUser(scanner); // 회원 삭제
+                    UserService.deleteUser(scanner); // 회원 삭제
                     break;
                 case 5:
-                    Exs_ch3_2_user_array_doc.addDummyUsers(); // 더미 데이터 추가
+                    UserService.addDummyUsers(); // 더미 데이터 추가
                     System.out.println("더미 데이터 5개가 추가되었습니다.");
                     break;
                 case 6:
-                    Exs_ch3_2_user_array_doc.searchUser(scanner); // 회원 검색
+                    UserService.searchUser(scanner); // 회원 검색
                     System.out.println("회원 검색 기능이 실행되었습니다.");
                     break;
                 case 0:
