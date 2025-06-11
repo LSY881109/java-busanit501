3bad64f3565946a21cb69f71eca8de0946252a31
이상용 <lsy3709@naver.com>
2025-06-11 16:59:23 +0900

    8교시, 파일 클래스 활용, 파일 삭제, 파일 존재 여부 확인, 폴더 생성

--- d250611/ch8_3/FileTest_Ex2.java
+++ d250611/ch8_3/FileTest_Ex2.java
@@ -13,12 +13,36 @@ public class FileTest_Ex2 {
         // System.out.println("파일 용량 확인 : " + file.length());
 
         // 3.현재 폴더의 모든 파일/폴더 이름 출력
-        File file = new File(".");
-        // 문자열 타입을 가지는 배열에, 파일, 폴더 명을 담기
-        String[] names = file.list();
-        // 출력
-        for (String string : names) {
-            System.out.println("현재 폴더 안에 파일명 폴더명 조회 : " + string);
+        // File file = new File("D:\\0-java-vscode");
+        // File file = new File(".");
+        // // 문자열 타입을 가지는 배열에, 파일, 폴더 명을 담기
+        // String[] names = file.list();
+        // // 출력
+        // for (String string : names) {
+        // System.out.println("현재 폴더 안에 파일명 폴더명 조회 : " + string);
+        // }
+
+        // 4. 특정 폴더가 없으면 새로 만들기
+        // File dir = new File("mydir");
+        // if (!dir.exists()) {
+        // dir.mkdir();
+        // }
+        // 5. 특정 파일 삭제
+        // File file = new File("test.jpg");
+        // if (file.isFile()) {
+        // file.delete();
+        // }
+        // 6. 해당 파일 있으면 삭제, 없으면 생성
+        try {
+            File file = new File("input.txt");
+            if (file.exists()) {
+                file.delete();
+            } else {
+                file.createNewFile();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 }
