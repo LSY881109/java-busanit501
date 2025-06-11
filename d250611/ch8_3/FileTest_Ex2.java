f291802f23dc2904d2f1ee36d66856f21da65684
이상용 <lsy3709@naver.com>
2025-06-11 16:36:40 +0900

    7교시, 파일 클래스 활용, 파일 크기 확인

--- d250611/ch8_3/FileTest_Ex2.java
+++ d250611/ch8_3/FileTest_Ex2.java
@@ -5,7 +5,11 @@ import java.io.File;
 public class FileTest_Ex2 {
     public static void main(String[] args) {
         // 새로운 폴더 만들기, backup 폴더 만들기.
-        File dir = new File("backup");
-        dir.mkdir();
+        // File dir = new File("backup");
+        // dir.mkdir();
+
+        // 2. 파일의 크기 확인 (용량 확인 )test.txt
+        File file = new File("test.txt");
+        System.out.println("파일 용량 확인 : " + file.length());
     }
 }
