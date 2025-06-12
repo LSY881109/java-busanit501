5f5c1188d0c571b5792349999ffeca2689ac75bb
이상용 <lsy3709@naver.com>
2025-06-12 16:06:07 +0900

    7교시, 배치관리자를 정렬하는 예시5, 회원 가입 구조 틀 예시2, 위치 조정

--- d250612/ch9_2/_6Container_Layout_Ex6.java
+++ d250612/ch9_2/_6Container_Layout_Ex6.java
@@ -19,6 +19,8 @@ public class _6Container_Layout_Ex6 {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("회원가입 간단구조  예시4");
+        // 프레임에 배치관리자 , BorderLayout 설정.
+        frame.setLayout(new BorderLayout());
 
         // 북, 회원 가입 라벨 붙이기,
         frame.add(new JLabel("회원 가입", SwingConstants.CENTER), BorderLayout.NORTH);
@@ -27,24 +29,18 @@ public class _6Container_Layout_Ex6 {
         JPanel centerPanel = new JPanel(new GridLayout(2, 2));
         centerPanel.add(new JLabel("이름"));
         centerPanel.add(new JTextField(20));
-        centerPanel.add(new JLabel("이메일"));
-        centerPanel.add(new JTextField(20));
         centerPanel.add(new JLabel("패스워드"));
         centerPanel.add(new JPasswordField(20));
-
-        frame.add(new JButton("가입"), BorderLayout.SOUTH);
-        // ======================================================================
-        // 프레임에 배치관리자 , BorderLayout 설정.
-        frame.setLayout(new BorderLayout());
-
         // 프레임에 -> 패널 붙이기 작업,
         frame.add(centerPanel, BorderLayout.CENTER);
 
+        frame.add(new JButton("가입"), BorderLayout.SOUTH);
+
         // ======================================================================
-        //
         // 창 크기, 창 닫기시 종료, 창 보이기 , 1세트
         frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        // ======================================================================
     }
 }
