de8609d6c8651ca477d6785d059633cec29361fd
이상용 <lsy3709@naver.com>
2025-06-13 11:38:05 +0900

    3교시, 회원 관리 프로그램 ver 3, 화면 작업, 검색어 , 테이블, 버튼 패널들 윈도우 창 배치 작업

--- d250613/web_structure/ui/_4SignupFrame.java
+++ d250613/web_structure/ui/_4SignupFrame.java
@@ -1,10 +1,13 @@
 package d250613.web_structure.ui;
 
+import java.awt.BorderLayout;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
@@ -35,14 +38,59 @@ public class _4SignupFrame extends JFrame {
 
     public _4SignupFrame() {
         setTitle("회원 가입");
-        setSize(400, 200);
+        setSize(700, 400);
         setLocationRelativeTo(null); // 화면에 중앙 배치.
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        // =================================================================
+        // 추가2.
+        // 테이블 작업 부터 진행 하기.
+        // 1) 테이블 헤더 만들기, 이름, 이메일, 패스워드, 가입일 ,
+        String[] cols = { "이름", "이메일", "패스워드", "가입일" };
+        tableModel = new DefaultTableModel(cols, 0) { // 익명클래스,
+            public boolean isCellEditable(int row, int column) {
+                return false; // 각테이블의 셀 클릭시 값이 변경이 되는 모드 기본, 안되게 변경함.
+            }
+        }; // 헤더제목, 0 빈테이블 생성,
+           // 표 형태의 데이터를 ,-> 화면 출력용 테이블에 데이터 연결.
+        memberTable = new JTable(tableModel);
+        // 보여주는 테이블에, 데이터 많아지면, 스크롤이 가능하게, 스크롤 기능 첨부,
+        JScrollPane scrollPane = new JScrollPane(memberTable); // 스크롤 기능이 있는 패널에 테이블 연결.
+
+        // 버튼 있는 패널 생성.
+        JPanel btnPanel = new JPanel(); // JPanel 의 기본 배치 관리자, FlowLayout ,나란히 배치.
+        JButton addBtn = new JButton("회원가입");
+        JButton updateBtn = new JButton("수정");
+        JButton deleteBtn = new JButton("삭제");
+        JButton reloadBtn = new JButton("새로고침");
+        // 버튼을 패널에 붙이기.
+        btnPanel.add(addBtn);
+        btnPanel.add(updateBtn);
+        btnPanel.add(deleteBtn);
+        btnPanel.add(reloadBtn);
+
+        // 검색 패널 생성
+        JPanel searchPanel = new JPanel();
+        searchField = new JTextField(20);
+        searchBtn = new JButton("검색");
+        resetBtn = new JButton("검색 초기화");
+        // 검색 패널에 버튼 붙이기 작업.
+        searchPanel.add(new JLabel("이름 또는 이메일 검색: "));
+        searchPanel.add(searchField);
+        searchPanel.add(searchBtn);
+        searchPanel.add(resetBtn);
+
+        // 레이아웃 배치 패널들 1) scrollPane : 데이터 테이블 2) btnPanel 버튼 3) searchPanel
+        setLayout(new BorderLayout());
+        add(searchPanel, BorderLayout.NORTH);
+        add(scrollPane, BorderLayout.CENTER);
+        add(btnPanel, BorderLayout.SOUTH);
+        // =================================================================
 
         // 안내 문구 표시
         JLabel label = new JLabel("여기는 회원 가입 화면입니다.", JLabel.CENTER);
         add(label);
-        setVisible(true);
 
     }
 
