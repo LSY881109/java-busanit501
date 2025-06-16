0af2932a49fb3b7738fed9a484c2eb2000c2e7c3
이상용 <lsy3709@naver.com>
2025-06-16 15:39:25 +0900

    6교시, JDBC, 실행 순서 개요6, 한명 회원 조회  작업 , 실행 시 오류 수정

--- d250616/ch10/_7JDBC_SelectOne.java
+++ d250616/ch10/_7JDBC_SelectOne.java
@@ -23,20 +23,24 @@ public class _7JDBC_SelectOne {
             // 조회,
             // int result = pstmt.executeUpdate();
             System.out.println("확인2 , pstmt.executeQuery(query); 코드 전");
-            rs = pstmt.executeQuery(query);
+            // 오류 1, 메서드에 쿼리 를 넣는 부분
+            rs = pstmt.executeQuery();
             System.out.println("확인3 , pstmt.executeQuery(query); 코드 후");
-            int id = rs.getInt("id");
-            String name = rs.getString("name");
-            String email = rs.getString("email");
-            String password2 = rs.getString("password");
-            String reg_date = rs.getString("reg_date");
-            // 콘솔에서, 데이터 조회 확인.
-            System.out.println("데이터 조회 : ");
-            System.out.println("id : " + id);
-            System.out.println("name : " + name);
-            System.out.println("email : " + email);
-            System.out.println("password2 : " + password2);
-            System.out.println("reg_date : " + reg_date);
+            // 오류 2, rs.next() 실행을 안해서
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String password2 = rs.getString("password");
+                String reg_date = rs.getString("reg_date");
+                // 콘솔에서, 데이터 조회 확인.
+                System.out.println("데이터 조회 : ");
+                System.out.println("id : " + id);
+                System.out.println("name : " + name);
+                System.out.println("email : " + email);
+                System.out.println("password2 : " + password2);
+                System.out.println("reg_date : " + reg_date);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
