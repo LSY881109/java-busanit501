ee4af82cecf3262116fec3c3a48c5b2fa1d208de
이상용 <lsy3709@naver.com>
2025-06-17 10:07:04 +0900

    2교시, 회원 관리 프로그램 ver4(디비연동), 전체 조회 기능 확인

--- d250617/web_structure/dao/_N1OracleMemberDAOImpl.java
+++ d250617/web_structure/dao/_N1OracleMemberDAOImpl.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import d250617.web_structure.dto._10Member;
@@ -26,24 +27,32 @@ public class _N1OracleMemberDAOImpl implements _9DAO_Inaterface {
         // 1) 드라이버 위치 2) 유저명 3) 패스워드 4) 디비서버의 아이피 및 포트 번호
         // 분리
 
+        // 디비에서 조회한 한명의 회원 -> 모델 클래스로 변환 -> 멤버를 타입으로 하는 리스트에 담기,
+        // 임시로 저장할 리스트
+        List<_10Member> memberList = new ArrayList<>();
         try {
             conn = _4DBConnectionManager.getConnection();
             String query = "SELECT * FROM MEMBER501 ORDER BY ID DESC";
             pstmt = conn.prepareStatement(query);
             rs = pstmt.executeQuery(query); // 실제 디비에 연결해서 실행 결과를 받아옴.
             while (rs.next()) {
+                // DB에서 한명의 회원 정보를 가져오기 -> 임시 변수 담기
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 String email = rs.getString("email");
                 String password2 = rs.getString("password");
                 String reg_date = rs.getString("reg_date");
+                // 멤버의 인스턴스 담기
+                _10Member member = new _10Member(id, name, email, password2, reg_date);
+                // 멤버 -> 리스트 담기
+                memberList.add(member);
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             _4DBConnectionManager.close(null, pstmt, conn);
         }
-        return null;
+        return memberList;
     }
 
     @Override
